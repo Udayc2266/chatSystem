4131899c71a48f3f22c16b844003980adb2aa185
Uday Chandra <udayc2266@gmail.com>
2025-03-03 01:54:09 +0530

    Update secondary.js

--- views/backend/secondary.js
+++ views/backend/secondary.js
@@ -2,7 +2,7 @@ const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const secondList = Schema({
+const secondList = new Schema({
     user : {
         type : Schema.Types.ObjectId,
         ref : "User"
